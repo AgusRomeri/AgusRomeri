e8d2649a46b77c5e8c222ce565bce7ffa8780f76
AgusRomeri <agusr.facultad@gmail.com>
2024-03-25 22:09:13 -0300

    Pase rotateleft y rotateright a myavltree

--- myavltree.py
+++ myavltree.py
@@ -13,7 +13,7 @@ class AVLNode:
     
 def search(tree,element):
     
-    node = searchnodeR(tree.root,element)
+    node = searchNodeR(tree.root,element)
     
     if node == None:
         return
@@ -188,6 +188,52 @@ def update(tree,element,key):
     else:
         node.value = element
         return node.key
+
+def rotateLeft(tree,avlnode):
+    
+    newroot = avlnode.rightnode
+    avlnode.rightnode = newroot.leftnode
+    
+    if newroot.leftnode != None:
+        newroot.leftnode.parent = avlnode
+    newroot.parent = avlnode.parent
+    
+    if avlnode.parent == None:
+        
+        tree.root = newroot    
+    elif avlnode.parent.leftnode == avlnode:
+        
+        avlnode.parent.leftnode == newroot
+    else:
+        
+        avlnode.parent.rightnode = newroot
+    
+    newroot.leftnode = avlnode
+    avlnode.parent = newroot
+
+def rotateRight(tree,avlnode):
+    
+    newroot = avlnode.leftnode
+    avlnode.leftnode = newroot.rightnode
+    
+    if newroot.rightnode != None:
+        
+        newroot.righnode.parent = avlnode
+    
+    newroot.parent = avlnode.parent
+    
+    if avlnode.parent == None:
+        
+        tree.root = newroot    
+    elif avlnode.parent.rightnode == avlnode:
+        
+        avlnode.parent.rightnode == newroot
+    else:
+        
+        avlnode.parent.leftnode = newroot
+    
+    newroot.rightnode = avlnode
+    avlnode.parent = newroot
 
 def balance(tree, node):
     if node is not None:
